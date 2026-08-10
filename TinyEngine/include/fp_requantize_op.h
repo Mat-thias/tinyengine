d67bfd69cb3a92932c319806a94a1dc380fe799a
Matthias <matthias@powerlabstech.com>
2026-08-10 23:00:17 +0100

    Added matrix multiplication def for 3-input kernels in fp_requantize_op.h

--- TinyEngine/include/fp_requantize_op.h
+++ TinyEngine/include/fp_requantize_op.h
@@ -80,6 +80,12 @@ q7_t* mat_mult_kernel_s8_s16_reordered_fpreq(const q7_t *input_a,
 		const int16_t activation_max, const uint16_t num_col_a,
 		const int32_t *const output_bias, q7_t *out_0);
 
+q7_t* mat_mult_kernel3_input3_s8_s16_fpreq(const q7_t *input_a,
+		const q15_t *input_b, const uint16_t output_ch, const float *scales,
+		const int32_t out_offset, const int16_t activation_min,
+		const int16_t activation_max, const uint16_t num_col_a,
+		const int32_t *const output_bias, q7_t *out_0, q15_t *kbuf);
+
 q7_t* mat_mult_kernel_s8_s16_reordered_ch8_fpreq(const q7_t *input_a,
 		const q15_t *input_b, const uint16_t output_ch, const float *scales,
 		const int32_t out_offset, const int16_t activation_min,
